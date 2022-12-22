11d7ed7ea4b0f77d39491baadc4e4b26bc4af1b4
Hakan "John" Gunerli <33205097+hakancangunerli@users.noreply.github.com>
2022-12-22 17:44:35 -0500

    somewhat completed, will look more into it

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # gt-weather-data
-look at the previous project from here:
 
+Using the weather station located at the Georgia Tech campus, this project aims to collect and analyze weather data. The data is collected using the API publicly available from the [Weather Underground](https://www.wunderground.com/). This information can be further developed into a webapp to provide weather information to the Georgia Tech community.
+## Images
 
+![](./cumulus.jpg)
 
-## References: 
-https://github.com/hakancangunerli/UGAWeatherData
+![](./gatecheast.jpg)
+
+![](./gatechsw.jpg)
+
+## References
+
+<https://github.com/hakancangunerli/UGAWeatherData>
---
+++ app.py
@@ -0,0 +1,61 @@
+# %%
+#https://cdn.weatherstem.com/dashboard/data/dynamic/model/gatech/stadium/latest.json
+
+import pandas as pd
+import requests
+import json
+import streamlit as st
+st.title("Georgia Tech Weather \"App\"")
+st.write("This is a weather app using data from Georgia Tech Weather Station")
+
+address = "https://cdn.weatherstem.com/dashboard/data/dynamic/model/gatech/stadium/latest.json"
+
+def get_data(address):
+    r = requests.get(address)
+    data = r.json()
+    return data
+
+# export this json file
+with open('gt.json', 'w') as f:
+    json.dump(get_data(address), f)
+
+df = pd.DataFrame(get_data(address))
+
+df.drop('validation_sensor', axis=1, inplace=True)
+
+# %%
+# we have three cameras in the stadium, gatechsw, gatecheast, and cumulus
+# https://gatech.weatherstem.com/skycamera/gatech/stadium/cumulus/snapshot.jpg
+# https://gatech.weatherstem.com/skycamera/gatech/stadium/gatecheast/snapshot.jpg
+
+cameras = ['gatechsw', 'gatecheast', 'cumulus']
+
+pictures = []
+
+for camera in cameras:
+    pictures.append(f"https://gatech.weatherstem.com/skycamera/gatech/stadium/{camera}/snapshot.jpg")
+
+
+# show the pictures side by side
+from IPython.display import Image, display
+
+for picture in pictures:
+    display(Image(url=picture, width=250))
+    # export images 
+    open (f"{picture.split('/')[-2]}.jpg", 'wb').write(requests.get(picture).content)
+
+st.write("Data from Georgia Tech Weather Station:")
+st.dataframe(df, width=1500, height=1000)
+# print temperature which is at index 14
+st.write("Temperature in Fahrenheit is:", df['records'][14]['value'])
+
+# print wind speed 
+st.write("Wind Speed in MPH is:", df['records'][4]['value'])
+
+#rainfall is at index 11 
+st.write("Rainfall in Inches is:", df['records'][11]['value'])
+
+
+# show images 
+st.write("Images from Georgia Tech Weather Station:")
+st.image(pictures)
