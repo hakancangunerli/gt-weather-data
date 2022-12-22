c52f73d234a910c55cb51dedda758c6230ce51c4
Hakan "John" Gunerli <33205097+hakancangunerli@users.noreply.github.com>
2022-12-22 18:06:50 -0500

    np ignore

--- app.py
+++ app.py
@@ -41,8 +41,8 @@ for camera in cameras:
 st.write("Data from Georgia Tech Weather Station:")
 st.write(df)
 
-# ignore bool warning
-pd.options.mode.chained_assignment = None
+# ignore bool warning from numpy 
+np.warnings.filterwarnings('ignore')
 
 # print temperature which is at index 14
 st.write("Temperature in Fahrenheit is:", df['records'][14]['value'])
