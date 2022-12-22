9b5e76e884a288f1f2d4583402be7be55b4be3ee
Hakan "John" Gunerli <33205097+hakancangunerli@users.noreply.github.com>
2022-12-22 18:03:58 -0500

    testing write

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ for camera in cameras:
 # show the pictures side by side
 
 st.write("Data from Georgia Tech Weather Station:")
-st.dataframe(df)
+st.write(df)
 
 # print temperature which is at index 14
 st.write("Temperature in Fahrenheit is:", df['records'][14]['value'])
